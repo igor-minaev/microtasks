3546144b868e21d4ab963242a8028359431b3ab3
Igor Minayeu <igor.minayeu@gmail.com>
2022-06-16 20:10:11 +0300

    add two buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <button></button>
-    </div>
-  );
+    // const myFirstSubscriber=(event: MouseEvent<HTMLButtonElement)=>{
+    //     console.log('Hello Im VASYA')
+    // }
+    // const mySecondSubscriber=()=>{
+    //     console.log('Hello Im Ivan')
+    // }
+
+    const onClickHandler=(name: string)=>{
+        console.log(name)
+    }
+
+
+    return (
+        <div className="App">
+            {/*<button onClick={(event)=>{console.log('Hello')}}>MyYoutube channel-1</button>*/}
+              <button onClick={()=>onClickHandler('VASYA')}>MyYoutube channel-1</button>
+              <button onClick={()=>onClickHandler('IVAN')}>MyYoutube channel-2</button>
+
+
+        </div>
+
+    );
 }
 
 export default App;
