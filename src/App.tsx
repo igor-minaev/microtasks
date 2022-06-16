bc5d07cbe46997363fa03d5a96f37b2f2d3a9250
Igor Minayeu <igor.minayeu@gmail.com>
2022-06-16 22:19:47 +0300

    add filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+type ButtonType= "All" | 'Dollars' | 'RUBLS'
 
 function App() {
     const [money, setMoney] = useState([
@@ -13,19 +14,45 @@ function App() {
         {banknots: 'Dollars', value: 50, number: ' x1234567890'},
         {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
     ])
-    return (
-        <ul>
-            {money.map((t, index)=>{
-                return(
-                    <li key={index}>
-                        <span>{t.banknots}</span>
-                        <span> {t.value}</span>
-                        <span> {t.number}</span>
 
-                    </li>
-                )
-            })}
-        </ul>
+    const [filter, setFilter] = useState('All')
+
+    let currentMoney = money
+    if(filter==='RUBLS'){
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'RUBLS')
+    }
+    if(filter==='Dollars'){
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === 'Dollars')
+    }
+
+
+    const onClickFilterHandler=(nameButton:string)=>{
+        setFilter(nameButton)
+    }
+
+
+    return (
+        <>
+            <ul>
+                {currentMoney.map((t, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{t.banknots}</span>
+                            <span> {t.value}</span>
+                            <span> {t.number}</span>
+                        </li>
+
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft: '35px'}}>
+                <button onClick={()=>onClickFilterHandler('All')}>All</button>
+                <button onClick={()=>onClickFilterHandler('Dollars')}>Dollars</button>
+                <button onClick={()=>onClickFilterHandler('RUBLS')}>RUBLS</button>
+            </div>
+        </>
+
+
     );
 }
 
