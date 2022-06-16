0399cfab32ebfb62ecac67f17fb8ba199cf14eb8
Igor Minayeu <igor.minayeu@gmail.com>
2022-06-16 21:12:30 +0300

    finish task

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,13 @@ function App() {
 
     // let a = 1
     let [a, setA] = useState(1)
-    const onClickHandler=()=>{
+    const onClickHandler = () => {
         setA(++a)
+
+        console.log(a)
+    }
+    const onClickHandler2 = () => {
+        setA(0)
 
         console.log(a)
     }
@@ -15,6 +20,7 @@ function App() {
         <div className="App">
             <h1>{a}</h1>
             <button onClick={onClickHandler}>number</button>
+            <button onClick={onClickHandler2}>0</button>
         </div>
     );
 }
