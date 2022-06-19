21deba6ac7064dcd152a7a83415c25423222e31d
Igor Minayeu <igor.minayeu@gmail.com>
2022-06-19 21:28:50 +0300

    finish task

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,40 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, {Fragment, useState} from 'react';
 import './App.css';
+import {Fullinput} from "./components/Fullinput";
+import {Input} from "./components/Input";
+import {Button} from "./components/Button";
 
 function App() {
-  let[message, setMessage]=useState([
-    {message: 'message1'},
-    {message: 'message2'},
-    {message: 'message3'}
-      ]
-  )
-  return (
-    <div className="App">
-      <div>
-        <input/>
-        <button>+</button>
-      </div>
-      {message.map((el,index)=>{
-        return(
-            <div key={index}>{el.message}</div>
-        )
-      })}
-    </div>
-  );
+    let [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'}
+        ]
+    )
+
+    let [title, setTitle] = useState('')
+    console.log(title)
+
+    const addMessage = (title: string) => {
+        let newMessage= {message: title}
+        setMessage([newMessage, ...message])
+    }
+    const callBackButtonHandler=()=>{
+        addMessage(title)
+        setTitle('')
+    }
+    return (
+        <div className="App">
+            {/*<Fullinput addMessage={addMessage}/>*/}
+            <Input setTitle={setTitle} title={title}/>
+            <Button name={'+'} callback={callBackButtonHandler} />
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
+    );
 }
 
 export default App;
--- src/components/Fullinput.tsx
+++ src/components/Fullinput.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 
-export const Fullinput = () => {
+type FullinputType = {
+    addMessage: (title: string) => void
+}
+export const Fullinput = (props: FullinputType) => {
+    let [title, setTitle] = useState('')
+
+
+    const onChangeInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setTitle(event.currentTarget.value)
+    }
+
+    const onClickButtonHandler = () => {
+        props.addMessage(title)
+        setTitle('')
+    }
     return (
         <div>
-            
+            <input value={title} onChange={onChangeInputHandler}/>
+            <button onClick={onClickButtonHandler}>+</button>
         </div>
     );
 };
